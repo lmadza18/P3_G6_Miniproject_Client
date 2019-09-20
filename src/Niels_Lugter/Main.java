309b3dcf36b4ce9d3f1464e3cbe4bbb743ca8687
Mikkel Kappel Persson <mperss18@student.aau.dk>
2019-09-20 16:05:34 +0200

    test

--- src/Niels_Lugter/Main.java
+++ src/Niels_Lugter/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,6 +16,11 @@ public class Main extends Application {
         primaryStage.setTitle("Band Room");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
+        ImageView imageView = new ImageView("images/stage.jpg");
+        imageView.setFitWidth(800);
+        imageView.setFitHeight(600);
+        root.getChildren().addAll(imageView);
     }
 
     public static void main(String[] args) {
