b71c7051002f31e4b95cc4e09fb84b2bd9d3ce35
Niels <Nielsr2@gmail.com>
2019-09-24 22:07:00 +0200

    Merge branch 'master' of /Users/n/School/MED3/Programming of Complex Software Systems/miniproject/P3_G6_Miniproject_Client with conflicts.

--- src/Niels_Lugter/Instrument.java
+++ src/Niels_Lugter/Instrument.java
@@ -39,6 +39,77 @@ public class Instrument {
                 case KeyEvent.VK_F:
                     break;
 
+                break;
+
+                case KeyEvent.VK_F:
+                    break;
+
+
+            }
+
+        }
+
+
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            printEventInfo("key pressed", e);
+
+
+            switch (e.getKeyCode()){
+
+                case KeyEvent.VK_A:
+                    break;
+
+                case KeyEvent.VK_S:
+                    break;
+
+                case KeyEvent.VK_D:
+                    break;
+
+                case KeyEvent.VK_F:
+                    break;
+
+            }
+
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            printEventInfo("key realesed", e);
+
+
+            switch (e.getKeyCode()){
+
+                case KeyEvent.VK_A:
+                    break;
+
+                case KeyEvent.VK_S:
+                    break;
+
+                case KeyEvent.VK_D:
+                    break;
+
+                case KeyEvent.VK_F:
+                    break;
+
+
+            }
+
+
+        }
+
+        private void printEventInfo() {
+            printEventInfo( );
+        }
+
+        private void printEventInfo(String key_pressed, KeyEvent e) {
+        }
+
+    }
+
 
             }
 
