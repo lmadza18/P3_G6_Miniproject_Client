b7e3c46a7aaa4b12659d189deb08b242a46ef861
malteerasmussen <malteerasmussen@gmail.com>
2019-09-25 14:21:33 +0200

    added /misc.xml to gitignore

--- src/Niels_Lugter/InstrumentPickerWindow.java
+++ src/Niels_Lugter/InstrumentPickerWindow.java
@@ -10,16 +10,12 @@ public class InstrumentPickerWindow extends Pane {
     Button closeButton;
     Button chooseButton;
 
-    double x;
-    double y;
-    double size = 250;
+    double x = getHeight()/2;
+    double y= getWidth()/2;
+    double size = this.getHeight()/2;
+
 
     public InstrumentPickerWindow(){
-
-
-    public InstrumentPickerWindow(double x, double y){
-        this.x = x;
-        this.y = y;
         Rectangle r = new Rectangle(x-size/2, y-size/2, size, size);
         r.setFill(Color.BLUE);
 
