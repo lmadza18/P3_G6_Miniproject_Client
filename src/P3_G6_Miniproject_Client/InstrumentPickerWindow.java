88a5c84c1e1836ea36ec72c81ddfa7944e513954
malteerasmussen <malteerasmussen@gmail.com>
2019-09-26 10:58:17 +0200

    Updated layout of Instrument Picker window

--- src/P3_G6_Miniproject_Client/InstrumentPickerWindow.java
+++ src/P3_G6_Miniproject_Client/InstrumentPickerWindow.java
@@ -1,31 +1,46 @@
 package P3_G6_Miniproject_Client;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class InstrumentPickerWindow extends VBox {
+public class InstrumentPickerWindow extends BorderPane {
 
     Button closeButton;
     Button chooseButton;
+    Button leftButton;
+    Button rightButton;
 
 
     double size = 200;
-    double x = 0;
+    double x = Main.root.getWidth()/2;
     double y= 0;
 
 
     public InstrumentPickerWindow(){
-        this.setAlignment(Pos.CENTER);
-        Rectangle r = new Rectangle(x ,y, size, size);
-        r.setFill(Color.BLUE);
+     //   Rectangle r = new Rectangle(size, size);
+     //   r.setFill(Color.BLUE);
+
+        ImageView img = new ImageView("images/drummer1.png");
 
         closeButton = new Button("close");
         chooseButton = new Button("choose");
+        leftButton = new Button("left");
+        rightButton = new Button("right");
 
-        this.getChildren().addAll(r, closeButton);
+        this.setCenter(img);
+        this.setTop(closeButton);
+        this.setBottom(chooseButton);
+        this.setLeft(leftButton);
+        this.setRight(rightButton);
+        setMargin(img, new Insets(50));
+        setAlignment(leftButton, Pos.CENTER);
+        setAlignment(rightButton, Pos.CENTER);
+        setAlignment(chooseButton,Pos.BOTTOM_RIGHT);
         buttonPress();
 
 
