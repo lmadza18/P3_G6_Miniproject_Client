a1fd7c7e9860c588517c46d01de2f6cece4cc0ed
MikkelKPersson <43847844+MikkelKPersson@users.noreply.github.com>
2019-09-28 21:40:41 +0200

    Added a spot instead of button

--- src/P3_G6_Miniproject_Client/RootUI.java
+++ src/P3_G6_Miniproject_Client/RootUI.java
@@ -1,11 +1,21 @@
 package P3_G6_Miniproject_Client;
 
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
+
 public class RootUI extends Pane {
+    StageSpot spot1;
+    StageSpot spot2;
+    StageSpot spot3;
+    StageSpot spot4;
+    Button getMyAssOuttaHere;
+
+
 
     RootUI() {
+
 
     }
 
@@ -14,12 +24,19 @@ public class RootUI extends Pane {
         imageView.setFitWidth(this.getWidth());
         imageView.setFitHeight(this.getHeight());
 
-        PosButton button1 = new PosButton("Position1", this.getWidth() / 8, this.getHeight() / 8 * 6);
-        PosButton button2 = new PosButton("Position2", this.getWidth() / 8 * 3, this.getHeight() / 8 * 5.5);
-        PosButton button3 = new PosButton("Position3", this.getWidth() / 8 * 5, this.getHeight() / 8 * 5.5);
-        PosButton button4 = new PosButton("Position4", this.getWidth() / 8 * 7, this.getHeight() / 8 * 6);
+        spot1 = new StageSpot(this.getWidth() / 8, this.getHeight() / 8 * 6);
+        spot2 = new StageSpot(this.getWidth() / 8 * 3, this.getHeight() / 8 * 5.5);
+        spot3 = new StageSpot(this.getWidth() / 8 * 5, this.getHeight() / 8 * 5.5);
+        spot4 = new StageSpot(this.getWidth() / 8 * 7, this.getHeight() / 8 * 6);
 
-        this.getChildren().addAll(imageView, button1, button2, button3, button4);
+        getMyAssOuttaHere = new Button("get my ass outta here!");
+
+//        PosButton button1 = new PosButton("Position1", this.getWidth() / 8, this.getHeight() / 8 * 6);
+//        PosButton button2 = new PosButton("Position2", this.getWidth() / 8 * 3, this.getHeight() / 8 * 5.5);
+//        PosButton button3 = new PosButton("Position3", this.getWidth() / 8 * 5, this.getHeight() / 8 * 5.5);
+//        PosButton button4 = new PosButton("Position4", this.getWidth() / 8 * 7, this.getHeight() / 8 * 6);
+
+        this.getChildren().addAll(imageView, spot1, spot2, spot3, spot4);
     }
 
 
