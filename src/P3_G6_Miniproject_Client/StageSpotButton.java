1fef9f47212cd3c3d4f335735a73e99102fc2ef2
Mikkel Kappel Persson <mperss18@student.aau.dk>
2019-10-17 13:15:59 +0200

    Added player when you choose spot

---
+++ src/P3_G6_Miniproject_Client/StageSpotButton.java
@@ -0,0 +1,59 @@
+package P3_G6_Miniproject_Client;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class StageSpotButton extends Pane {
+
+    Button button;
+    ImageView buttonImg;
+    ImageView buttonHoverImg;
+    int imageSize;
+
+    double x;
+    double y;
+
+    //Put button in here
+    StageSpotButton() {
+
+        imageSize = (int) Math.round(Main.root.getWidth() / 10);
+        int buttonSize = Math.round(imageSize / 6 * 4);
+        int buttonTransform = Math.round(imageSize / 6);
+        buttonImg = new ImageView("Takespot_button.png");
+        buttonHoverImg = new ImageView("Takespot_button_hover.png");
+
+        buttonImg.setFitWidth(imageSize);
+        buttonImg.setFitHeight(imageSize);
+        buttonHoverImg.setFitWidth(imageSize);
+        buttonHoverImg.setFitHeight(imageSize);
+
+        button = new Button(null);
+        button.setStyle("-fx-background-color: transparent");
+        button.setMinSize(buttonSize, buttonSize);
+        button.setTranslateX(buttonTransform);
+        button.setTranslateY(buttonTransform);
+
+        getChildren().addAll(buttonImg, buttonHoverImg, button);
+
+        //button.setPickOnBounds(true);
+        buttonHoverImg.setVisible(false);
+
+        button.setOnMouseEntered(t -> {
+            buttonImg.setVisible(false);
+            buttonHoverImg.setVisible(true);
+        });
+
+        button.setOnMouseExited(t -> {
+            buttonImg.setVisible(true);
+            buttonHoverImg.setVisible(false);
+        });
+
+//        this.x = x - (imageSize / 2);
+//        this.y = y - (imageSize / 2);
+//        this.setTranslateX(this.x);
+//        this.setTranslateY(this.y);
+
+
+    }
+}
