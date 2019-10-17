00cc5751e2551e62c859202efb53d632bc9bb1bb
Niels <Nielsr2@gmail.com>
2019-10-17 15:26:26 +0200

    updated /test to /hello to get playerID

--- src/P3_G6_Miniproject_Client/Main.java
+++ src/P3_G6_Miniproject_Client/Main.java
@@ -19,6 +19,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+
+
         // ------------------------------------------- OSC TEST
         final OSCClient c;
 
